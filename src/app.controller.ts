93475af92af7e9daa6a11a025e4e093aa36074b4
vladimirverh <vladimir@unique.ch>
2023-02-08 20:18:28 +0300

    Test axios v 2.0.0

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const test = await this.httpService.get('https://www.google.com/').toPromise();
+    console.log(`TEST: ${JSON.stringify(test.statusText)}`);
     return this.appService.getHello();
   }
 }
